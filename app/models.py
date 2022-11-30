5cb4b431f2f7c68bc511a93e676b3bdaed08e387
admin <masharpik@gmail.com>
2022-11-30 21:15:44 +0300

    Добавила комментарии старого получения данных, чтобы проверить, правильно ли перенеста изменения дз2

--- app/models.py
+++ app/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# ANSWERS_FOR_QUESTIONS = [
+#     {
+#         'id': id,
+#         'text': f"Text of answer #{id + 1}",
+#         'is_correct': bool(id % 2),
+#         'score': id % 3
+#     }
+#     for id in range(21)
+# ]
+
+# QUESTIONS = [
+#     {
+#         'id': id,
+#         'title': f"Question #{id + 1}",
+#         'text': f"Text of question #{id + 1}",
+#         'count_answers': id % 3 + 1,
+#         'tags': ['bender', 'milk', 'python'],
+#         'score': id % 4,
+#         'answers': ANSWERS_FOR_QUESTIONS
+#     }
+#     for id in range(66)
+# ]
 
 class ProfileManager(models.Manager):
     pass

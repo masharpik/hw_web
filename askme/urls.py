0abe1a693874f8f29dd67bbf91f2535494a76ec4
admin <masharpik@gmail.com>
2022-11-30 21:10:24 +0300

    Было неудобно, но переместила исправления дз2 в дз3

--- askme/urls.py
+++ askme/urls.py
@@ -1,5 +1,4 @@
 """askme URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -14,18 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('hot', views.hot, name='hot'),
-    path('tag/<str:tag_name>', views.tag, name='tag'),
-    path('ask', views.ask, name='ask'),
-    path('login', views.login, name='login'),
-    path('question/<int:question_id>', views.question, name='question'),
-    path('signup', views.registration, name='registration'),
-    path('settings', views.settings, name='settings')
+    path('', include('app.urls'))
 ]
